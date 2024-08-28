9fe9fd0869da174e008f21f79a51f113e2690e6f
Amirmask <134484611+Amirmask@users.noreply.github.com>
2024-08-28 17:17:06 -0300

    Add files via upload

---
+++ proyecto etapa 2/datos.js
@@ -0,0 +1,39 @@
+let productos = [
+ {
+    id: 1,
+    nombre: "azucar",
+    descripcion: "xxxxxx",
+    precio: 1.000,
+    cantidad: 5 
+ },
+ {
+    id: 2,
+    nombre: "miel",
+    descripcion: "xxxxxx",
+    precio: 500,
+    cantidad: 8
+ },
+ {
+    id: 3,
+    nombre: "leche",
+    descripcion: "xxxxxx",
+    precio: 2.000,
+    cantidad: 2
+ },
+ {
+    id: 4,
+    nombre: "turron",
+    descripcion: "xxxxxx",
+    precio: 200,
+    cantidad: 5
+ },
+ {
+    id: 5,
+    nombre: "xxxxx",
+    descripcion: "xxxxxx",
+    precio: 1.000,
+    cantidad:5
+ }
+];
+
+export default productos;
